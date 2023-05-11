10df34807c639c1f8b872a9e61131006fe430944
mohamedgamal_MG <mohamedgamal.jemmy.zero@gmail.com>
2023-05-11 04:53:28 -0700

    ARGUMANT

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+count = len(argv) - 1
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(count))
+for i in range(count):
+    print("{}: {}".format(i + 1, argv[i + 1]))
