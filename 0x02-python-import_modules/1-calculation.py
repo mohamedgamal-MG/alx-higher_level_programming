54394a20f383473fa9c17ef53dd812a6cb570e79
mohamedgamal_MG <mohamedgamal.jemmy.zero@gmail.com>
2023-05-11 02:56:00 -0700

    CALCULATION

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,8 @@
 if __name__ == '__main__':
     import calculator_1 as cal
 
-    a, b = 10, 5
+    a = 10
+    b = 5
     print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
