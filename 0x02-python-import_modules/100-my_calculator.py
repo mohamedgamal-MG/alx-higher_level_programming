5c8837166b0c25c8bdd7b4cdff3daf2a9a85ebb4
mohamedgamal_MG <mohamedgamal.jemmy.zero@gmail.com>
2023-05-11 06:20:55 -0700

    my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import calculator_1, sys
+    import calculator_1
+    from sys import argv, exit
 
-    count = len(sys.argv) - 1
+    count = len(argv) - 1
     if count < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
     elif count == 3:
-        if sys.argv[2] == "+":
-            result = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], result))
-        elif sys.argv[2] == "-":
-            result = calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], result))
-        elif sys.argv[2] == "*":
-            result = calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], result))
-        elif sys.argv[2] == "/":
-            result = calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], result))
+        if argv[2] == "+":
+            result = calculator_1.add(int(argv[1]), int(argv[3]))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
+        elif argv[2] == "-":
+            result = calculator_1.sub(int(argv[1]), int(argv[3]))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
+        elif argv[2] == "*":
+            result = calculator_1.mul(int(argv[1]), int(argv[3]))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
+        elif argv[2] == "/":
+            result = calculator_1.div(int(argv[1]), int(argv[3]))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], result))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
