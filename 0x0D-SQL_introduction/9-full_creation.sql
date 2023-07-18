0dc2209f923dd9de1b2ae63affc46e6586e9f0e1
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-07-18 12:43:37 -0700

    new table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- this script for creat a table seconf in database.
-CREATE TABLE IF NOT EXISTS `seconde_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `seconde_table` (`id`, `name`, `score`) VALUES (1, "Johan", 10)
-INSERT INTO `seconde_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3)
-INSERT INTO `seconde_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14)
-INSERT INTO `seconde_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "Johan", 10)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
 
