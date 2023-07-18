cdc07ae45f56820e4ae7ebcf7eef94bb3eb03b6c
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-07-18 09:42:58 -0700

    full-table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- this script will be craere a teable in mysql server .
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
 
