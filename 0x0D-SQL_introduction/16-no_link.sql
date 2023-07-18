96dafe9cdcec77df3c46b3ba89fd30cc873ded9a
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-07-18 14:06:42 -0700

    SQL

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- script for list all records of the table.
-SELECT `secore`, `name`
+SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != ""
-ORDER BY `score` DESC;
+ORDER BY `score` DESC
 
