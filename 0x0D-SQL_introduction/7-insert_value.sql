02b96e22d4bf5a83b45431a7fdac69647077c4a0
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-07-18 12:10:12 -0700

    update

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- this script to insert anew row in the table.
-INSERT INTO `first_table`
-VALUES (`id` 89, `name` Best School);
+INSERT INTO `first_table` (`id`, `name`)
+VALUES (89, Best School);
 
