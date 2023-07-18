e6a649d92c3171378f8555c2c4203633073c8f08
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-07-18 12:28:03 -0700

    new table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- this script for creat a table seconf in database.
+CREATE TABLE IF NOT EXISTS seconde_table (id INT, name VARCHAR(256), score INT)
+INSERT INTO seconde_table (id, name, score) 
+VALUES (1, "Johan", 10)
+VALUES (2, "Alex", 3)
+VALUES (3, "Bob", 14)
+VALUES (4, "George", 8);
+
