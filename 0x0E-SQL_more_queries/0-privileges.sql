63f674260cee0029f9d710da3bdfffc3f9596e18
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-07-19 08:34:27 -0700

    02-create_user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,4 +2,3 @@
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
-
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,1 +1,3 @@
-
+-- this script for creat mysql usrer.
+CREATE USER IF EXISTS user_0d_1@localhost IDENTFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
