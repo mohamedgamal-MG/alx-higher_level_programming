f3ad25cfddbe6dfd475d7e340b26dc27a1f178bc
Mohamed Gamal <120610309+mohamedgamal-MG@users.noreply.github.com>
2023-08-15 01:13:00 +0300

    2-arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const argc = process.argv.length - 2;
+const argc = process.argv.length;
 
-if (argc === 0){
-	console.log('No argument');
-} else if (argc === 1) {
-	console.log('Argument found');
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-	console.log('Arguments found');
+  console.log('No argument');
 }
