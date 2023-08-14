8bf2c9e3f75a16642902c0434ef69f97c3e00c6a
Mohamed Gamal <120610309+mohamedgamal-MG@users.noreply.github.com>
2023-08-15 00:40:21 +0300

    1-3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 //use console.log(...) to print all output
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
