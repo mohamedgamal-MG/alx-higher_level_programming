20db037b88f8c8da17d3d41ec2da56c46f08e4ec
Mohamed Gamal <120610309+mohamedgamal-MG@users.noreply.github.com>
2023-08-15 01:02:12 +0300

    2-arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-//Get the number of commind-line argiments passed
+//Get the number of command-line arguments passed
 // Subtract 2 to exclude the "node" and script path arguments
 // Print a message based on the number of arguments
-const argsCount = process.argv.length - 2;
-if (argsCount === 0) {
-	console.log('No argument');
-} else if (argsCount === 1) {
-	console.log('Argument found');
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-	console.log('Argument found')
+  console.log('Arguments found');
 }
