eb73b18d60f8fd8a12e0f34b92dcb8b954fe9ff0
mohamedgamal_MG <mohamedgamal.jemmy.zero@gmail.com>
2023-08-14 21:13:47 +0000

    0-first constant,frist print

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
+// Create constant variable called myVar with value "JavaScript is amazing"
+// Use console.log(..) to print the output
 
-//Define a conistant variable calles myVar with the value "JavaScript is amazing"
 const myVar = 'JavaScript is amazing';
-
-//Print the value of myVar to the console
 console.log(myVar);
