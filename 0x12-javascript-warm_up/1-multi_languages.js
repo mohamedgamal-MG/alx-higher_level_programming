071deba3e23c4ad5c4a56367093c2e1468c7816b
mohamedgamal_MG <mohamedgamal.jemmy.zero@gmail.com>
2023-08-14 21:32:30 +0000

    1-3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 //The print 3 lines with this script
-console.log("C is fun \nPython is cool \nJavaScript is amazing");
+const myVar = "C is fun \nPython is cool \nJavaScript is amazing"
+console.log(myVar);
