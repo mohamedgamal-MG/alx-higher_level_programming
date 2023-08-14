e5df162b3c74d28f6fdfae168620b3309328ad07
mohamedgamal_MG <mohamedgamal.jemmy.zero@gmail.com>
2023-08-14 21:08:25 +0000

    0-first constant,frist print

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+//Define a conistant variable calles myVar with the value "JavaScript is amazing"
+const myVar = "JavaScript is amazing";
+//Print the value of myVar to the console
+console.log(myVar);
