2fc7b88d10f3cc1a0348bf5da8d74604b625ce88
Mohamed Gamal <120610309+mohamedgamal-MG@users.noreply.github.com>
2023-08-16 03:20:41 +0300

    update

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+// Get the source file paths and the destination file path from command line arguments
+// Read the contents of the source files
+// Concatenate the contents of the source files
+// Write the concatenated content to the destination file
+
 const fs = require('fs');
 
 const fArg = fs.readFileSync(process.argv[2]).toString();
