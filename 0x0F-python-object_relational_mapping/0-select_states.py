9dbb483ff90a905d9ff4adf97e63ea7c2f5a70fe
Mohamed Gamal <120610309+mohamedgamal-MG@users.noreply.github.com>
2023-08-21 03:15:39 +0300

    updata

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,24 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(sys.argv) != 5:
+        print("Usage: {} <host> <user> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+        
+    host = sys.argv[1]
+    user = sys.argv[2]
+    password = sys.argv[3]
+    database = sys.argv[4]
+    
+    try:
+        db = MySQLdb.connect(host=host, user=user, passwd=password, db=database, port=3306)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
