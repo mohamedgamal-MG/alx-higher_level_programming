e47c09aa750897c528db751617bd737a6617d937
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-08-22 04:38:26 -0700

    0-select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+def get_states(username, password, database):
+    try:
+        # Establishing a connection to the MySQL server
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        
+        # Creating a cursor object to execute queries
+        cur = conn.cursor()
+        
+        # Executing the SELECT query to retrieve states
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        
+        # Fetching all rows from the query result
+        query_rows = cur.fetchall()
+        
+        # Displaying the results
+        for row in query_rows:
+            print(row)
+        
+        # Closing the cursor and the connection
+        cur.close()
+        conn.close()
+    except MySQLdb.Error as e:
+        print("Error:", e)
+        sys.exit(1)
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    
+    get_states(username, password, database)
+
