1ad7455ce31f1520e58a07b965874b4894b6fbc6
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-09-25 23:59:05 -0700

    3-starwars_title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/';
+
+request (API_URL + episodeNum, function(err, response, body) {
+	if (err){
+		console.log(err);
+	} else if (response.statusCode === 200){
+		const filmData = JSON.parse(body);
+		console.log(filmData.title);
+	} else {
+		console.log('Error code: ' + response.statusCode);
+	}
+});
+
