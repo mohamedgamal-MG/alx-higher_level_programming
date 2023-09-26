7f96f755c216661b20e246c3eec36ca9416f8f41
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-09-26 13:55:33 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs')
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8',  error => {
-	if(error) console.log(error);
+
+const fs = require('fs');
+const filename = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filename, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
 });
 
